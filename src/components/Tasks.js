669a104954c4dff04b5156c06d970b52677ac483
Jasterix <jasterix19@gmail.com>
2022-02-02 12:04:49 -0500

    Create example tasks

---
+++ src/components/Tasks.js
@@ -0,0 +1,22 @@
+const tasks = [{
+    id:1,
+    text: "Appointment",
+    day: "Feb 2 at 4pm",
+    reminder: true,
+},
+{
+    id: 2,
+    text: "Visit",
+    day: "Feb 9th at 6pm",
+    reminder: false
+}]
+
+const Tasks = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Tasks
